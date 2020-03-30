6f2b8299cd4909b9e645245630ac435c6b8407a9
Rodrigo Suélli <rodrigosuellli@gmail.com>
2020-03-30 20:43:40 -0300

    style: Change style

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,7 +6,7 @@ import Header from '../../components/header';
 import Sidebar from '../../components/sidebar';
 
 export default function Main() {
-    const [menuOpen, SetmenuOpen] = useState(true);
+    const [menuOpen, SetmenuOpen] = useState(false);
 
     function handleMenu () {
         if (menuOpen === false){
@@ -17,18 +17,27 @@ export default function Main() {
     }
         return(
             <div id="main-container" className="main-container">
-                <Header click={handleMenu}/>
+                <Header click={handleMenu} />
                 <Sidebar isOpen={menuOpen} />
                 <section id="content" className="content">
-                <h1 className="slogan">
-                    O <span className="ditey">ditey</span> dita qualquer coisa
-                    <span className="y-dot">.</span>
-                    <span className="r-dot">.</span>
-                    <span className="g-dot">.</span>
-                </h1>
-                <textarea placeholder="Insira seu texto aqui...">
+                    <h1 className="slogan">
+                        O <span className="ditey">ditey</span> dita qualquer coisa
+                        <span className="y-dot">.</span>
+                        <span className="r-dot">.</span>
+                        <span className="g-dot">.</span>
+                    </h1>
 
-                </textarea>
+                    <section className="main">
+                    <textarea placeholder="Insira seu texto aqui..."></textarea>
+                    <div className="commands">
+                        
+                    </div>
+                    <div className="settings">
+                    <label htmlFor="speed">Speed:</label>
+                    <input type="range" name="speed"/>
+                    </div>
+                    </section>
+
                 </section>
             </div>
         );
