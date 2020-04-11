1fae9f560dafd68515b3b76dd49f6159d24f400b
Rodrigo Suélli <rodrigosuellli@gmail.com>
2020-04-11 20:58:48 -0300

    fix: Hamburger button

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { Link } from 'react-router-dom';
 
 import './styles.css'
 
-function Header() {
+export default function Header() {
+    const [menu, setMenu] = useState(false);
+
+    function handleToggleMenu() {
+        if (menu === false) {
+            document.body.style.overflow = "hidden";
+            return setMenu(true);
+        }
+        
+        document.body.style.overflow = "initial";
+        return setMenu(false);
+    }
+
+    let className = 'menu-container';
+    if (menu === true) {
+            className += ' on';
+    }
+
     return(
-        <header>
+        <header className={className}>
             <div className="logo-container">
-                <div className="hamburger">
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
+                <div onClick={handleToggleMenu} 
+                className="hamburger"
+                >
+                    <div className="one"></div>
+                    <div className="two"></div>
+                    <div className="three"></div>
                 </div>
                 <Link className="logo" to="/">
                 <span className="logo-brand">ditey</span>
@@ -27,15 +46,13 @@ function Header() {
                      <Link className="nav-link" to="#">quem somos</Link>
                  </li>
                  <li className="nav-item">
-                     <Link className="nav-link" to="#">avalie</Link>
+                     <Link className="nav-link" to="#">nosso app</Link>
                  </li>
                  <li className="nav-item">
-                     <Link className="nav-link" to="#">nosso app</Link>
+                     <Link className="nav-link" to="#">avalie</Link>
                  </li>
              </ul>
             </nav>
         </header>
     );
 }
-
-export default Header;
