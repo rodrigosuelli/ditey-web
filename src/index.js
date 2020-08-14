9908dbcd3737d6af5f477785c570323768d4868d
Rodrigo <rodrigosuellli@gmail.com>
2020-08-14 15:33:24 -0300

    fix: import global.css

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 
+import './global.css';
+
 ReactDOM.render(<Routes />, document.getElementById('root'));
