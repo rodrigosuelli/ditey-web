c30489db04338efb6c5ef13d9e58febd2189a33f
Rodrigo <rodrigosuellli@gmail.com>
2020-09-30 20:52:00 -0300

    feat: make progress in toolbar component

--- src/components/Toolbar/Sidebar/index.js
+++ src/components/Toolbar/Sidebar/index.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { MdClose } from 'react-icons/md';
+import { MdArrowDropDown } from 'react-icons/md';
 
 import miniLogoImg from '../../../assets/svg/ditey-minilogo.svg';
 
 import './styles.css';
 
 export default function Sidebar({ menu, setMenu, onMenuToggle }) {
-  let className = '';
-  if (menu === true) className = 'visible';
+  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+  const { name, email } = userInfo;
+  const nameInitial = name[0].toUpperCase();
+
+  let className = 'toolbar';
+  if (menu === true) className += ' visible';
 
   return (
     <aside className={className}>
-      <div className="brand">
-        <Link to="/">
-          <img src={miniLogoImg} alt="miniLogo" />
-        </Link>
-        <button onClick={onMenuToggle} className="close-btn" type="button">
-          <MdClose color="#fff" size={28} />
-        </button>
+      <div className="profile">
+        <div className="profile-img">{nameInitial}</div>
+        <div className="profile-button">
+          <div className="profile-info">
+            <h4>{name}</h4>
+            <p>{email}</p>
+          </div>
+
+          <MdArrowDropDown size={24} />
+        </div>
       </div>
-      <div className="links">
+      <div className="texts-list">
         <Link to="/">Home</Link>
         <Link to="/register">Cadastro</Link>
         <Link to="/login">Log in</Link>
