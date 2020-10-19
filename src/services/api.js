7ba8cd05de434bf2d87d94a7da196d569a3da5cf
Rodrigo <rodrigosuellli@gmail.com>
2020-10-18 21:41:55 -0300

    Select axios baseURL based on ENV environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
+let url;
+
+if (process.env.ENV !== 'dev') {
+  url = 'http://192.168.0.108:3333/api';
+} else {
+  url = 'https://ditey-api-deploy.herokuapp.com/api';
+}
+
 const api = axios.create({
-  baseURL: 'https://ditey-api-deploy.herokuapp.com/api',
+  baseURL: url,
 });
 
 export default api;
