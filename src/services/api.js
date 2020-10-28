df1b6a8b2e65b17c0d0db813d38538fb16d8fafc
Rodrigo <rodrigosuellli@gmail.com>
2020-10-28 18:02:15 -0300

    Replace local machine ip with localhost

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let url;
 
 if (process.env.NODE_ENV === 'development') {
-  url = 'http://192.168.0.108:3333/api';
+  url = 'http://localhost:3333/api';
 } else {
   url = 'https://ditey-api-deploy.herokuapp.com/api';
 }
